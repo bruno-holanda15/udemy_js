2490a3cc39f2d1c9b2df6a3554bb3e128f442d94
Bruno Holanda <br.holanda15@gmail.com>
2021-06-10 16:36:30 -0300

    Initializing repository

---
+++ estruturas_controle/dowhile.js
@@ -0,0 +1,16 @@
+function getInteiroAleatorioEntre(min, max) {
+    
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+
+}
+
+let opcao = -1
+
+do {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção sorteada na função foi ${opcao}`)   
+    
+} while (opcao != -1); 
+
+console.log('Fim')
---
+++ estruturas_controle/ifelse.js
@@ -0,0 +1,13 @@
+const imprimirResultado = function (nota) {
+    if (nota >= 7 ) {
+        console.log('Parabéns!')
+    } else {
+        console.log('Errouu!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!') // cuidado 
+
+
